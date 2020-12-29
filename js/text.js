50a772e2453c67577fcc354464b2f8415ccf9515
nirav24 <npatel@kinaxis.com>
2020-12-29 11:09:36 -0500

    ADD: various links

---
+++ js/text.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#info-text', {
+    strings: [
+        "I&acute;m a Software Engineer ^100",
+        "I work with microservices, containerization, and DevOps",
+        "I&acute;m proficient in JavaScript, Python, Kubernetes"],
+    typeSpeed: 50,
+    loop: true,
+    showCursor: false
+  });
